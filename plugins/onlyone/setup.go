df6ea20ffc4f5c8827394f460558c6514bdb066b
shao <shaoqing@zdns.cn>
2024-05-09 16:38:21 +0800

    logs

--- plugins/onlyone/setup.go
+++ plugins/onlyone/setup.go
@@ -10,6 +10,7 @@ import (
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/cpusoft/goutil/belogs"
 	_ "github.com/cpusoft/goutil/conf"
 	_ "github.com/cpusoft/goutil/logs"
 	"github.com/miekg/dns"
@@ -18,7 +19,7 @@ import (
 var log = clog.NewWithPlugin("onlyone")
 
 func init() {
-	log.Info("init()")
+	belogs.Info("init()")
 	caddy.RegisterPlugin("onlyone", caddy.Plugin{
 		ServerType: "dns",
 		Action:     setup,
@@ -26,15 +27,15 @@ func init() {
 }
 
 func setup(c *caddy.Controller) error {
-	log.Info("setup()")
+	belogs.Info("setup()")
 	t, err := parse(c)
 	if err != nil {
 		return plugin.Error("onlyone", err)
 	}
-	log.Info("setup(): t:", t)
+	belogs.Info("setup(): t:", t)
 
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		log.Info("setup(): AddPlugin t:", t)
+		belogs.Info("setup(): AddPlugin t:", t)
 		t.Next = next
 		return t
 	})
@@ -43,7 +44,7 @@ func setup(c *caddy.Controller) error {
 }
 
 func parse(c *caddy.Controller) (*onlyone, error) {
-	log.Info("parse():")
+	belogs.Info("parse():")
 	o := &onlyone{types: typeMap{dns.TypeA: true, dns.TypeAAAA: true},
 		pick: rand.Intn}
 
@@ -51,7 +52,7 @@ func parse(c *caddy.Controller) (*onlyone, error) {
 	for c.Next() {
 		// onlyone should just be in the server block once.
 		if found {
-			log.Info("parse(): Next and found")
+			belogs.Info("parse(): Next and found")
 			return nil, plugin.ErrOnce
 		}
 		found = true
@@ -59,17 +60,17 @@ func parse(c *caddy.Controller) (*onlyone, error) {
 		// parse the zone list, normalizing each to a FQDN, and
 		// using the zones from the server block if none are given.
 		args := c.RemainingArgs()
-		log.Info("parse(): args:", args)
+		belogs.Info("parse(): args:", args)
 		if len(args) == 0 {
 			o.zones = make([]string, len(c.ServerBlockKeys))
 			copy(o.zones, c.ServerBlockKeys)
 		}
 		for _, str := range args {
-			log.Info("parse(): range args, str:", str)
+			belogs.Info("parse(): range args, str:", str)
 			o.zones = append(o.zones, plugin.Host(str).Normalize())
 		}
-		log.Info("parse(): o.zones:", o.zones)
-		log.Info("parse(): c.Val():", c.Val())
+		belogs.Info("parse(): o.zones:", o.zones)
+		belogs.Info("parse(): c.Val():", c.Val())
 		for c.NextBlock() {
 			switch c.Val() {
 			case "types":
@@ -79,10 +80,10 @@ func parse(c *caddy.Controller) (*onlyone, error) {
 						"at least one type must be listed")
 				}
 				o.types = make(typeMap, len(args))
-				log.Info("parse(): o.types:", o.types)
+				belogs.Info("parse(): o.types:", o.types)
 				for _, a := range args {
 					t, ok := dns.StringToType[strings.ToUpper(a)]
-					log.Info("parse(): range args, a:", a)
+					belogs.Info("parse(): range args, a:", a)
 					if !ok {
 						return nil,
 							fmt.Errorf("invalid type %q",
